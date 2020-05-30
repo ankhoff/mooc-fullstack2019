e315859b63f50b51229f6c6de3f49f11381beb17
ankhoff <43796186+ankhoff@users.noreply.github.com>
2020-05-30 20:01:08 +0300

    2.11: puhelinluettelo step6 Removed initial state from persons Added eventHandler Added useEffect

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,25 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Rows from './components/Rows'
+import axios from 'axios'
 import FilterForm from './components/FilterForm'
 import AddPersonForm from './components/AddPersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newFilter, setNewFilter] = useState('')
+  const [persons, setPersons] = useState( [] )
+  const [newName, setNewName] = useState( '' )
+  const [newNumber, setNewNumber] = useState( '' )
+  const [newFilter, setNewFilter] = useState( '' )
 
-  const addNewPerson = (event) => {
+  const eventHandler = response => {
+    console.log('response.data: ', response.data)
+    setPersons(response.data)
+  }
+
+  useEffect( () => {
+    axios
+      .get( 'http://localhost:3001/persons' )
+      .then( eventHandler )
+  }, [] )
+
+  const addNewPerson = ( event ) => {
     event.preventDefault()
 
-    if (persons.some(record => record.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
-      setNewName('')
+    if ( persons.some( record => record.name === newName ) ) {
+      alert( `${newName} is already added to phonebook` )
+      setNewName( '' )
       return
     }
 
@@ -28,23 +35,23 @@ const App = () => {
       number: newNumber
     }
 
-    setPersons(persons.concat(person))
-    setNewName('')
-    setNewNumber('')
+    setPersons( persons.concat( person ) )
+    setNewName( '' )
+    setNewNumber( '' )
 
   }
 
-  const createNewName = (event) => {
-    setNewName(event.target.value)
+  const createNewName = ( event ) => {
+    setNewName( event.target.value )
   }
 
-  const createNewNumber = (event) => {
-    setNewNumber(event.target.value)
+  const createNewNumber = ( event ) => {
+    setNewNumber( event.target.value )
   }
 
-  const createNewFilter = (event) => {
-    console.log(event.target.value)
-    setNewFilter(event.target.value)
+  const createNewFilter = ( event ) => {
+    console.log( event.target.value )
+    setNewFilter( event.target.value )
   }
 
   //const Person = ({ name, phone }) => <div>{name} {phone}</div>
@@ -54,11 +61,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <FilterForm newFilter={newFilter} createNewFilter={createNewFilter} />
+      <FilterForm newFilter={ newFilter } createNewFilter={ createNewFilter } />
       <h2>Add new</h2>
-      <AddPersonForm newName={newName} newNumber={newNumber} addNewPerson={addNewPerson} createNewName={createNewName} createNewNumber={createNewNumber} />
+      <AddPersonForm newName={ newName } newNumber={ newNumber } addNewPerson={ addNewPerson } createNewName={ createNewName } createNewNumber={ createNewNumber } />
       <h2>Numbers</h2>
-      <Rows persons={persons} newFilter={newFilter} />
+      <Rows persons={ persons } newFilter={ newFilter } />
     </div>
 
   )
