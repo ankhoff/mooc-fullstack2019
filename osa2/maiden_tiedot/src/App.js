ea7611bade637ee0772a602a11c750b6427d224a
ankhoff <43796186+ankhoff@users.noreply.github.com>
2021-02-26 19:09:15 +0200

    minor fixes, formatting change

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,48 +1,48 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import FindCountryForm from './components/FindCountryForm'
 import Result from './components/Result'
 import axios from 'axios'
 
-require( 'dotenv' ).config()
+require('dotenv').config()
 
 function App () {
-  const [countries, setCountries] = useState( [] )
-  const [searchTerm, setSearchTerm] = useState( '' )
-  const [searchResult, setSearchResult] = useState( [] )
-  const [weather, setWeather] = useState( {} )
+  const [countries, setCountries] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [searchResult, setSearchResult] = useState([])
+  const [weather, setWeather] = useState({})
 
   const api_key = process.env.REACT_APP_API_KEY
   const baseURL = 'http://api.weatherstack.com/'
   const pack = 'current'
 
-  useEffect( () => {
+  useEffect(() => {
     axios
-      .get( 'https://restcountries.eu/rest/v2/all' )
-      .then( response => {
-        setCountries( response.data )
-      } )
-  }, [] )
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
 
-  useEffect( () => {
-    if ( searchResult.length === 1 ) {
+  useEffect(() => {
+    if (searchResult.length === 1) {
       axios
-        .get( baseURL + pack + '?access_key=' + api_key + '&query=' + searchResult[0].capital )
-        .then( response => {
-          setWeather( response.data )
-        } )
+        .get(baseURL + pack + '?access_key=' + api_key + '&query=' + searchResult[0].capital)
+        .then(response => {
+          setWeather(response.data)
+        })
     }
-  }, [searchResult] )
+  }, [searchResult])
 
-  const newSearchTerm = ( event ) => {
-    const searchTermMatches = countries.filter( country => country.name.toLowerCase().includes( event.target.value.toLowerCase() ) )
-    setSearchTerm( event.target.value )
-    setSearchResult( searchTermMatches );
+  const newSearchTerm = (event) => {
+    const searchTermMatches = countries.filter(country => country.name.toLowerCase().includes(event.target.value.toLowerCase()))
+    setSearchTerm(event.target.value)
+    setSearchResult(searchTermMatches);
   }
 
   return (
     <div>
-      <FindCountryForm searchTerm={ searchTerm } createNewSearchTerm={ newSearchTerm } />
-      <Result countries={ searchResult } manualSet={ newSearchTerm } weather={ weather } />
+      <FindCountryForm searchTerm={searchTerm} createNewSearchTerm={newSearchTerm} />
+      <Result countries={searchResult} manualSet={newSearchTerm} weather={weather} />
     </div>
   );
 }
